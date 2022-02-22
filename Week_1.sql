25c5d548fda0ab88e025b3bc948fce3d221ff8e7
RedDevils <60249099+chinmay002@users.noreply.github.com>
2022-02-22 12:31:52 +0530

    Update Week_1.sql

--- Week_1.sql
+++ Week_1.sql
@@ -57,3 +57,56 @@ order by 2 desc
 limit 1) top_id
 )
 group by 1
+
+
+
+----------------------------------------------------------------------------
+-- 6. Which item was purchased first by the customer after they became a member?
+with cte as (
+select s.customer_id,s.order_date,s.product_id,m.join_date ,
+rank() over (partition by s.customer_id order by extract(day from s.order_date::timestamp - m.join_date::timestamp)asc) as rnk
+from dannys_diner.sales s
+inner join dannys_diner.members m
+on s.customer_id=m.customer_id
+and s.order_date>=m.join_date) 
+
+select c.customer_id,m.product_name
+from cte c join dannys_diner.menu m
+on c.product_id=m.product_id
+where rnk =1
+
+----------------------------------------------------------------------------
+
+-- 7. Which item was purchased just before the customer became a member?
+
+with cte as
+(select s.customer_id,s.order_date,s.product_id,m.join_date ,
+rank() over (partition by s.customer_id order by s.order_date desc) as rnk
+from dannys_diner.sales s
+inner join dannys_diner.members m
+on s.customer_id=m.customer_id
+and s.order_date<m.join_date
+)
+select c.customer_id,m.product_name
+from cte c join dannys_diner.menu m
+on c.product_id=m.product_id
+where rnk =1
+
+----------------------------------------------------------------------------
+
+-- 8. What is the total items and amount spent for each member before they became a member?
+
+with cte as (
+select s.customer_id,s.order_date,s.product_id,men.price
+from dannys_diner.sales s
+inner join dannys_diner.members m
+on s.customer_id=m.customer_id
+and s.order_date<m.join_date
+inner join dannys_diner.menu men
+on s.product_id=men.product_id
+)
+select customer_id,count(product_id),sum(price)
+from cte
+group by 1
+
+----------------------------------------------------------------------------
